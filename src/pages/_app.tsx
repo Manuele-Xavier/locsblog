ec42af104d72aec4557e2d9ab87309f736072b0b
unknown <manu.mouraamd@gmail.com>
2024-08-29 21:50:39 -0300

    config apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,17 @@ import '@/styles/globals.css';
 import Header from "@/components/header";
 import Footer from "@/components/footer"
 
-const App = ({Component, pageProps}: AppProps)=>{
-    return(
-      <div>
-        <Header/>
-        <Component {...pageProps}/>
-        <Footer/>
-      </div>
-    )
-  }
-  export default App
+import { ApolloProvider } from '@apollo/client';
+import client from '@/apollo-client'
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div>
+      <Header />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+      <Footer />
+    </div>
+  )
+}
+export default App
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,16 +1,44 @@
 import Link  from "next/link"
+import { useEffect, useState } from 'react';
 import { Post } from "@/types/types"
+import { gql } from '@apollo/client';
+import client from "@/apollo-client";
+
+
+const GET_POSTS = gql`
+ query MyQuery {
+  posts {
+    nodes {
+      date
+      title
+      content
+      id
+      slug
+    }
+  }
+}
+`;
 const Posts = () => {
-   
-    const posts: Post[] = [
-        { id: '1', title: 'First Post', content: 'Content of the first post' },
-        { id: '2', title: 'Second Post', content: 'Content of the second post' },
-    ]
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const fetchPosts = async () => {
+          try {
+            const { data } = await client.query({
+              query: GET_POSTS,
+            });
+            setPosts(data.posts.nodes); // Atualiza o estado com os posts
+          } catch (error) {
+            console.error('Erro ao buscar posts:', error);
+          }
+        };
+    
+        fetchPosts(); 
+      }, [client]);
     return (
-        <div>
+        <div className="flex-col">
             <h1>Posts</h1>
             {posts.map((post:Post)=>(
-                <Link href={`/posts/${post.id}`}>
+                <Link href={`/posts/${post.slug}`}>
                      {post.title}
                 </Link>
             ))}
